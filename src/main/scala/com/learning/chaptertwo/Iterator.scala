91fcd06edba8796247ff428f91268f51d4b55e4d
worasit daimongkol <worasit.dmk501@gmail.com>
2017-05-07 19:10:38 +0700

    Added chapter 2

--- src/main/scala/com/learning/chaptertwo/Iterator.scala
+++ src/main/scala/com/learning/chaptertwo/Iterator.scala
@@ -1,16 +1,24 @@
 package com.learning.chaptertwo
 
+import scala.io.{BufferedSource, Source}
 
-object Iterator extends App with Description {
+
+object Iterator extends Description {
+
   initDescription()
 
+  val source: BufferedSource = Source.fromResource("Chapter 2/TX417945_1263.csv")
 
+
+
+  source.close
 }
 
 
 trait Description {
 
   lazy val section: String = "Section 2, Lesson 25"
+
 
   def initDescription(): String = {
     println(section)
